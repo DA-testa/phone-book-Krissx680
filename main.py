8bf8db637c798273a8fc03cc1abb7fe853a73889
221RDB362 <122871094+Krissx680@users.noreply.github.com>
2023-03-31 09:19:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# python3
-
 class Query:
     def __init__(self, query):
         self.type = query[0]
@@ -41,5 +39,6 @@ def process_queries(queries):
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
+
 
 
