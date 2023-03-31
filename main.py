cef0fe802c07328910d8a78195f107c10f5db531
221RDB362 <122871094+Krissx680@users.noreply.github.com>
2023-03-31 08:46:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ class Query:
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
+        elif self.type == 'update':
+            self.name = query[2]
 
 def read_queries():
     n = int(input())
@@ -19,9 +21,15 @@ def process_queries(queries):
     # Keep list of all existing (i.e. not deleted yet) contacts.
     contacts = []
     for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
+        if cur_query.type == 'add' or cur_query.type == 'update':
+            contacts[cur_query.number] = cur_query.name
+        elif cur_query.type == 'del':
+            if cur_query.number in contacts:
+                del contacts[cur_query.number]
+        else:
+            response = contacts.get(cur_query.number, 'not found')
+            result.append(response)
+    return result
             for contact in contacts:
                 if contact.number == cur_query.number:
                     contact.name = cur_query.name
